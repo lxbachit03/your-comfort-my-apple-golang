5dd6bbc31bb014091bf90279453c84561a71b73c
bachlex03 <lxbachit03.working@gmail.com>
2026-06-01 17:20:00 +0700

    setup route that allows get path param

--- main.go
+++ main.go
@@ -15,6 +15,15 @@ func main() {
 		})
 	})
 
+	r.GET("/languages/:lang", func(ctx *gin.Context) {
+		pathParam := ctx.Param("lang")
+
+		ctx.JSON(200, gin.H{
+			"message": "languages route",
+			"path":    pathParam,
+		})
+	})
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
