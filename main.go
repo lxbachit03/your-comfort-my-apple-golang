f19f7b78cfee6d99523bb16428528b351d28d7e6
bachlex03 <lxbachit03.working@gmail.com>
2026-06-01 16:13:27 +0700

    init Gin framework

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+}
