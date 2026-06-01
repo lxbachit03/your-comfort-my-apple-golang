e43e3333c4f6be27109c9c2bfeb38a9a825882e8
B4$ch.lee16 <162127302+lxbachit03@users.noreply.github.com>
2026-06-01 17:47:57 +0700

    Merge pull request #5 from lxbachit03/feature/4-setup-api-rountes
    Feature/4 setup api rountes

--- main.go
+++ main.go
@@ -9,6 +9,34 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.GET("/demo", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "demo routes",
+		})
+	})
+
+	r.GET("/languages/:lang", func(ctx *gin.Context) {
+		pathParam := ctx.Param("lang")
+
+		ctx.JSON(200, gin.H{
+			"message": "languages route",
+			"path":    pathParam,
+		})
+	})
+
+	r.GET("/language/golang", func(ctx *gin.Context) {
+
+		price := ctx.Query("price")
+		level := ctx.Query("level")
+
+		ctx.JSON(200, gin.H{
+			"message": "golang route",
+			"path":    "golang",
+			"price":   price,
+			"level":   level,
+		})
+	})
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
