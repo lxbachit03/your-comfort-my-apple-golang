38131b9bc4d2cb9a35bb55979ada8b63821cfafc
bachlex03 <lxbachit03.working@gmail.com>
2026-06-01 17:08:20 +0700

    setup simple route

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.GET("/demo", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "demo routes",
+		})
+	})
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
