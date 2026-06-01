21f23d4652a2da46bd58d24881ab848e852e6b94
bachlex03 <lxbachit03.working@gmail.com>
2026-06-01 17:23:31 +0700

    feat(api-routes): setup route that allows to get query params (#4)

--- main.go
+++ main.go
@@ -24,6 +24,19 @@ func main() {
 		})
 	})
 
+	r.GET("/language/golang", func(ctx *gin.Context) {
+
+		price := ctx.Query("price")
+		level := ctx.Query("level")
+
+		ctx.JSON(200, gin.H{
+			"message": "golang route",
+			"path":    "golang",
+			"price":   price,
+			"level":   level,
+		})
+	})
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
