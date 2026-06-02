34782149d276bf6b3d0bde194babfa6c20a00f52
B4$ch.lee16 <162127302+lxbachit03@users.noreply.github.com>
2026-06-03 00:11:46 +0700

    feat(api-route): validate request body (#8)

--- utils/validate-formater.go
+++ utils/validate-formater.go
@@ -13,26 +13,49 @@ func FormatValidationError(err error) gin.H {
 		errorsMap := make(map[string]string)
 
 		for _, e := range errors {
+
+			root := strings.Split(e.Namespace(), ".")[0]
+
+			path := strings.TrimPrefix(e.Namespace(), root+".")
+
+			parts := strings.Split(path, ".")
+
+			for i, part := range parts {
+				if strings.Contains(part, "[") {
+					idx := strings.Index(part, "[")
+					base := part[:idx]
+					numberIdx := part[idx:]
+
+					parts[i] = CamelCaseToSnakeCase(base) + numberIdx
+				} else {
+					parts[i] = CamelCaseToSnakeCase(part)
+				}
+			}
+
+			fieldName := strings.Join(parts, ".")
+
 			switch e.Tag() {
 			case "uuid":
-				errorsMap[e.Field()] = "Invalid format (must be UUID)"
+				errorsMap[fieldName] = "Invalid format (must be UUID)"
+			case "required":
+				errorsMap[fieldName] = "This field is required"
 			case "gt":
-				errorsMap[e.Field()] = fmt.Sprintf("Must be greater than %v", e.Param())
+				errorsMap[fieldName] = fmt.Sprintf("Must be greater than %v", e.Param())
 			case "lte":
-				errorsMap[e.Field()] = fmt.Sprintf("Must be less than or equal to %v", e.Param())
+				errorsMap[fieldName] = fmt.Sprintf("Must be less than or equal to %v", e.Param())
 			case "gte":
-				errorsMap[e.Field()] = fmt.Sprintf("Must be greater than or equal to %v", e.Param())
+				errorsMap[fieldName] = fmt.Sprintf("Must be greater than or equal to %v", e.Param())
 			case "datetime":
-				errorsMap[e.Field()] = fmt.Sprintf("Invalid date format (must be %v)", e.Param())
+				errorsMap[fieldName] = fmt.Sprintf("Invalid date format (must be %v)", e.Param())
 			case "slug":
-				errorsMap[e.Field()] = "Invalid slug format"
+				errorsMap[fieldName] = "Invalid slug format"
 			case "oneof":
 				allowedList := strings.Join(strings.Split(e.Param(), " "), ", ")
-				errorsMap[e.Field()] = fmt.Sprintf("Value must be one of: %v", allowedList)
+				errorsMap[fieldName] = fmt.Sprintf("Value must be one of: %v", allowedList)
 			case "search":
-				errorsMap[e.Field()] = "Search query only accepts letters, numbers, and spaces"
+				errorsMap[fieldName] = "Search query only accepts letters, numbers, and spaces"
 			default:
-				errorsMap[e.Field()] = fmt.Sprintf("Invalid value %v on %s tag", e.Value(), e.Tag())
+				errorsMap[fieldName] = fmt.Sprintf("Invalid value %v on %s tag", e.Value(), e.Tag())
 			}
 		}
 
