a2bcbfbfed11c1a30c1cc017ce13f5fadf371e7f
bachlex03 <lxbachit03.working@gmail.com>
2026-06-02 14:36:45 +0700

    feat: api versioning

--- main.go
+++ main.go
@@ -6,36 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func GetUsersV1(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "get users v1",
+	})
+}
+
+func GetUsersV2(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "get users v2",
+	})
+}
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/demo", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "demo routes",
-		})
-	})
-
-	r.GET("/languages/:lang", func(ctx *gin.Context) {
-		pathParam := ctx.Param("lang")
-
-		ctx.JSON(200, gin.H{
-			"message": "languages route",
-			"path":    pathParam,
-		})
-	})
-
-	r.GET("/language/golang", func(ctx *gin.Context) {
-
-		price := ctx.Query("price")
-		level := ctx.Query("level")
-
-		ctx.JSON(200, gin.H{
-			"message": "golang route",
-			"path":    "golang",
-			"price":   price,
-			"level":   level,
-		})
-	})
+	r.GET("api/v1/users", GetUsersV1)
+	r.GET("api/v2/users", GetUsersV2)
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
