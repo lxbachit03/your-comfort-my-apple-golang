e6a170adc4f9501019061a658716ad7e11c8c104
bachlex03 <lxbachit03.working@gmail.com>
2026-06-03 19:13:06 +0700

    integrate logger middleware

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,14 +1,65 @@
 package middlewares
 
 import (
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
 
 func LoggerMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
 
-		logsPath = "logs/http.log"
+	logsPath := "logs/http.log"
+
+	if err := os.MkdirAll(filepath.Dir(logsPath), os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	logFile, err := os.OpenFile(logsPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	logger := zerolog.New(logFile).With().Timestamp().Logger()
+
+	return func(ctx *gin.Context) {
+		logEvent := logger.Info()
+
+		startedAt := time.Now()
 
 		ctx.Next()
+
+		statusCode := ctx.Writer.Status()
+
+		if statusCode >= 500 {
+			logEvent = logger.Error()
+		} else if statusCode >= 400 {
+			logEvent = logger.Warn()
+		} else {
+			logEvent = logger.Info()
+		}
+
+		logEvent.
+			Str("method", ctx.Request.Method).
+			Str("path", ctx.Request.URL.Path).
+			Str("query", ctx.Request.URL.RawQuery).
+			Str("client_ip", ctx.ClientIP()).
+			Str("user_agent", ctx.Request.UserAgent()).
+			Str("referer", ctx.Request.Referer()).
+			Str("protocol", ctx.Request.Proto).
+			Str("host", ctx.Request.Host).
+			Str("remote_addr", ctx.Request.RemoteAddr).
+			Str("request_uri", ctx.Request.RequestURI).
+			Int64("content_length", ctx.Request.ContentLength).
+			Interface("headers", ctx.Request.Header).
+			// Interface("request_body", requestBody).
+			Int("status_code", statusCode).
+			// Interface("response_body", responseBodyParsed).
+			Int64("duration_ms", time.Since(startedAt).Milliseconds()).
+			Msg("HTTP Request Log")
+
 	}
 }
