d0a099abdf3bd6d703877de8ec7c483315b21c00
bachlex03 <lxbachit03.working@gmail.com>
2026-06-08 17:01:37 +0700

    update packages dependency

--- internal/pkg/logger/logger.go
+++ internal/pkg/logger/logger.go
@@ -1,6 +1,17 @@
 package logger
 
-import "github.com/rs/zerolog"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/natefinch/lumberjack"
+	"github.com/rs/zerolog"
+)
 
 type LoggerConfig struct {
 	Level      string
@@ -12,8 +23,63 @@ type LoggerConfig struct {
 	IsDev      string
 }
 
+type PrettyJSONWriter struct {
+	Writer io.Writer
+}
+
+type contextKey string
+
+const TraceIdKey contextKey = "trace_id"
+
 var Log *zerolog.Logger
 
 func NewLogger(config LoggerConfig) {
+	zerolog.TimeFieldFormat = time.RFC3339
 
+	// lvl, err := zerolog.ParseLevel(config.Level)
+	// if err != nil {
+	// 	lvl = zerolog.InfoLevel
+	// }
+	// zerolog.SetGlobalLevel(lvl)
+
+	var writer io.Writer
+
+	if config.IsDev == "development" {
+		if strings.Contains(config.Filename, "app.log") {
+			writer = zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+		} else {
+			writer = PrettyJSONWriter{Writer: os.Stdout}
+		}
+	} else {
+		writer = &lumberjack.Logger{
+			Filename:   config.Filename,
+			MaxSize:    config.MaxSize,
+			MaxBackups: config.MaxBackups,
+			MaxAge:     config.MaxAge,
+			Compress:   config.Compress,
+		}
+	}
+
+	logger := zerolog.New(writer).With().Timestamp().Logger()
+
+	Log = &logger
+}
+
+func (w PrettyJSONWriter) Write(p []byte) (n int, err error) {
+	var prettyJSON bytes.Buffer
+
+	err = json.Indent(&prettyJSON, p, "", "  ")
+	if err != nil {
+		return w.Writer.Write(p)
+	}
+
+	return w.Writer.Write(prettyJSON.Bytes())
+}
+
+func GetTraceID(ctx context.Context) string {
+	if traceID, ok := ctx.Value(TraceIdKey).(string); ok {
+		return traceID
+	}
+
+	return ""
 }
